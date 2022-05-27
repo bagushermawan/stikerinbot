d7dfa6ccef76d9d601775e77942c8c4e3b3c4491
bagushermawan <bagusherma123@gmail.com>
2022-05-27 10:59:47 +0700

    first

--- config.js
+++ config.js
@@ -1,52 +1,55 @@
-let fs = require('fs')
-let chalk = require('chalk')
+let fs = require("fs");
+let chalk = require("chalk");
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ["6281547223631"]; // Letakan nomor kamu disini
 
-global.APIs = { // API Prefix
+global.APIs = {
+  // API Prefix
   // nama: 'https://website'
-  amel: 'https://melcanz.com',
-  bx: 'https://bx-hunter.herokuapp.com',
-  rey: 'https://api.reysekha.my.id',
-  dhnjing: 'https://dhnjing.xyz',
-  hardianto: 'https://hardianto.xyz',
-  neoxr: 'https://api.neoxr.eu.org',
-  nrtm: 'https://nurutomo.herokuapp.com',
-  pencarikode: 'https://api.chipa.xyz',
-  waifupics: 'https://api.waifu.pics',
-  xteam: 'https://api.xteam.xyz',
-  zeks: 'https://api.zeks.me',
-}
-global.APIKeys = { // APIKey nya disini
+  amel: "https://melcanz.com",
+  bx: "https://bx-hunter.herokuapp.com",
+  rey: "https://api.reysekha.my.id",
+  dhnjing: "https://dhnjing.xyz",
+  hardianto: "https://hardianto.xyz",
+  neoxr: "https://api.neoxr.eu.org",
+  nrtm: "https://nurutomo.herokuapp.com",
+  pencarikode: "https://api.chipa.xyz",
+  waifupics: "https://api.waifu.pics",
+  xteam: "https://api.xteam.xyz",
+  zeks: "https://api.zeks.me",
+};
+global.APIKeys = {
+  // APIKey nya disini
   // 'https://website': 'apikey'
-  'https://melcanz.com': 'trial',
-  'https://api.reysekha.my.id': 'apirey',
-  'https://bx-hunter.herokuapp.com': 'Ikyy69',
-  'https://hardianto.xyz': 'hardianto',
-  'https://api.neoxr.eu.org': 'yntkts',
-  'https://api.chipa.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
-  'https://api.zeks.me': 'apivinz',
-}
+  "https://melcanz.com": "trial",
+  "https://api.reysekha.my.id": "apirey",
+  "https://bx-hunter.herokuapp.com": "Ikyy69",
+  "https://hardianto.xyz": "hardianto",
+  "https://api.neoxr.eu.org": "yntkts",
+  "https://api.chipa.xyz": "pais",
+  "https://api.xteam.xyz": "apikeymu",
+  "https://api.zeks.me": "apivinz",
+};
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = "yo";
+global.author = "her";
 
-global.wm = '© ariffb & melcanz'
-global.wait = '_*tunggu sedang diproses...*_'
-global.eror = '_*Server Error*_'
-global.benar = '✅'
-global.salah = '❌'
-global.dikit = 'dikit lagi'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
+global.wm = "herrr";
+global.wait = "_*tunggu sedang diproses...*_";
+global.eror = "_*Server Error*_";
+global.benar = "✅";
+global.salah = "❌";
+global.dikit = "dikit lagi";
+global.fla =
+  "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=";
 
-global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 69; // Semakin tinggi, semakin sulit naik level
 
-let file = require.resolve(__filename)
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-  fs.unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  delete require.cache[file]
-  require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  delete require.cache[file];
+  require(file);
+});
--- plugins/stickmaker.js
+++ plugins/stickmaker.js
@@ -1,14 +1,15 @@
 //made by Anshul
-const uploadImage = require('../lib/uploadImage')
-const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
-const effects = ['jail', 'gay', 'glass', 'wasted', 'triggered']
+const uploadImage = require("../lib/uploadImage");
+const { sticker } = require("../lib/sticker");
+const { MessageType } = require("@adiwajshing/baileys");
+const effects = ["jail", "gay", "glass", "wasted", "triggered"];
 
 let handler = async (m, { conn, usedPrefix, text, command }) => {
-  let effect = text.trim().toLowerCase()
-  if (!effects.includes(effect)) throw `
+  let effect = text.trim().toLowerCase();
+  if (!effects.includes(effect))
+    throw `
 ┌─「 *Daftar Efek * 」
-${effects.map(effect => `├ ${effect}`).join('\n')}
+${effects.map((effect) => `├ ${effect}`).join("\n")}
 └────
 
 Penggunaan:
@@ -16,25 +17,31 @@ ${usedPrefix + command} <efek>
 
 Contoh:
 ${usedPrefix + command} jail
-`.trim()
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Balas gambar dengan perintah *${usedPrefix + command}*`
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Media tidak didukung!`
-  let img = await q.download()
-  let url = await uploadImage(img)
-  let apiUrl = API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
-    avatar: url
-  })
+`.trim();
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
+  if (!mime) throw `Balas gambar dengan perintah *${usedPrefix + command}*`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Media tidak didukung!`;
+  let img = await q.download();
+  let url = await uploadImage(img);
+  let apiUrl = API(
+    "https://some-random-api.ml/canvas/",
+    encodeURIComponent(effect),
+    {
+      avatar: url,
+    }
+  );
   try {
-    let stiker = await sticker(null, apiUrl, packname, author)
-    await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
+    let stiker = await sticker(null, apiUrl, packname, author);
+    await conn.sendFile(m.chat, stiker, "", "", m, 0, { asSticker: true });
   } catch (e) {
-    await conn.sendFile(m.chat, apiUrl, 'image.png', null, m, 0, { thumbnail: await (await fetch(apiUrl)).buffer() })
+    await conn.sendFile(m.chat, apiUrl, "image.png", null, m, 0, {
+      thumbnail: await (await fetch(apiUrl)).buffer(),
+    });
   }
-}
-handler.help = ['stikermaker']
-handler.tags = ['sticker']
-handler.command = /^(s(tic?ker)?maker)$/i
+};
+handler.help = ["stikermaker"];
+// handler.tags = ['sticker']
+handler.command = /^(s(tic?ker)?maker)$/i;
 
-module.exports = handler
+module.exports = handler;
